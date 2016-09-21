54cff1e1c251e91e7a10f27472c2eb8329179d74
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-09-21 15:54:44 +0200

    finished aggregate file parser

--- tix_aggregate_parser/tix_aggregate_parser.sh
+++ tix_aggregate_parser/tix_aggregate_parser.sh
@@ -13,5 +13,26 @@ then
   echo "There is no script: $SCRIPT"
   exit
 fi
-echo $CMD
+
+for csv in $CSVS
+do
+  echo "Doing $csv"
+  OUTFL="${csv%.csv}_parsed.tsv"
+  cut -f1,8,9,12,13,14,15,16,18,27,29 $csv > $OUTFL
+done
+
+
+TSVS=`find $PTH -name "*tsv"`
+OUTF="${PTH}/target_infect_x_library_layouts.tsv"
+echo "Outfile: ${OUTF}"
+touch $OUTF
+for tsv in $TSVS
+do
+  IFS=' ' read -ra CLS <<<  `head -n 1 ${tsv} | wc | tr -s " "`
+  echo "Column count $tsv: ${CLS[1]}"
+  echo `head -n 1 ${tsv}`
+  echo ""
+  cat $tsv >> $OUTF
+done
+
 
