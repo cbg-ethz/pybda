9a338df4562e2fba5fbe9c79324d2237fef67c89
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-09-22 21:19:41 +0200

    added python project for parsing

--- tix_plate_parser/python/main.py
+++ tix_plate_parser/python/main.py
@@ -3,12 +3,33 @@
 # __date__   = 22/09/16
 
 from __future__ import print_function, absolute_import
+import sys
 import argparse
+
+from .matlab_parser import MatlabParser
 
 
 def parse_options(args):
     parser = argparse.ArgumentParser(description='Parse matlab files of genetic perturbation screens.')
-    parser.add_argument('-f', type=str, help='input file', required=True, metavar='input-file')
-    parser.add_argument('-s', type=str, help='size of graph', required=True, metavar='size')
+    parser.add_argument('-f',
+                        type=str,
+                        help='input folder, e.g. BRUCELLA-AU-CV3/VZ001-2H',
+                        required=True,
+                        metavar='input-folder')
+    parser.add_argument('-m',
+                        type=str,
+                        help='meta file, e.g. target_infect_x_library_layouts_beautified.tsv',
+                        required=True,
+                        metavar='meta-file')
     opts = parser.parse_args(args)
-    return opts.f, opts.s
+    return opts.f, opts.m
+
+
+def main(args):
+    fold, meta = parse_options(args)
+    parser = MatlabParser(fold, meta)
+    parser.parse(fold, meta)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
---
+++ tix_plate_parser/python/matlab_parser.py
@@ -0,0 +1,11 @@
+# __author__ = 'Simon Dirmeier'
+# __email__  = 'simon.dirmeier@bsse.ethz.ch'
+# __date__   = 22/09/16
+
+class MatlabParser:
+    def __init__(self, folder, meta):
+        self._folder = folder
+        self._meta = meta
+
+    def parse(self):
+        pass
