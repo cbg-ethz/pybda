689b57a126321c62af7e1d831411d5b7910ab8cd
dirmeier <simon.dirmeier@bsse.ethz.ch>
2016-10-29 00:11:18 +0200

    moved palte loader to other class

--- tix_plate_parser/python/plate_parser/plate_parser.py
+++ tix_plate_parser/python/plate_parser/plate_parser.py
@@ -4,13 +4,13 @@
 
 import logging
 import numpy
+import subprocess
 import multiprocessing as mp
 
 from .plate_experiment_meta import PlateExperimentMeta
 from .plate_layout import PlateLayoutMeta
 from ._plate_sirna_gene_mapping import PlateSirnaGeneMapping
 from ._utility import load_matlab
-from .plate_loader import PlateLoader
 from ._plate_cell_features import PlateCellFeature
 from .plate_file_set_parser import PlateFileSetParser
 
@@ -18,6 +18,7 @@ logger = mp.log_to_stderr()
 logger.setLevel(logging.INFO)
 
 lock, pool = None, None
+
 
 class PlateParser:
     """
@@ -235,3 +236,52 @@ class PlateParser:
                             iimg + 1, cell + 1]
                     f.write("\t".join(list(map(str, meta)) +
                                       list(map(str, vals))).lower() + "\n")
+
+
+class PlateLoader:
+    """
+    Class for plate downloading.
+
+    """
+
+    def __init__(self, bee_loader, output_path, username, pw):
+        """
+        Constructor for the plate downloader.
+
+        :param bee_loader: the full path with the script name that downloads
+        the data (e.g. /bla/..../BeeDataSetDownloader.sh)
+        :param output_path: the folder where all the plates get stored and
+        parsed to
+        :param username: the user name of the open bis instance
+        :param pw: the password of the open bis instance
+        """
+        self._bee_loader = bee_loader
+        self._output_path = output_path
+        self._username = username
+        self._pw = pw
+
+    def load(self, plate_id):
+        """
+        Download a plate using the bee-data downloader.
+
+        :param plate_id: the full qualifier id of a plate
+        :return:
+        """
+        global lock
+        try:
+            lock.acquire()
+            logger.info("Downloading: " + plate_id)
+            sc = [self._bee_loader,
+                  "--user", self._username,
+                  "--password", self._pw,
+                  "--outputdir", self._output_path,
+                  "--plateid", plate_id,
+                  "--type", "HCS_ANALYSIS_CELL_FEATURES_CC_MAT",
+                  "--newest",
+                  "--files", ".*.mat"]
+            ret = subprocess.call(sc)
+            if ret != 0:
+                logger.warn("\tdownload failed with status: " + str(0))
+        finally:
+            lock.release()
+        return ret
