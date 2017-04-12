bbd871fae7d47aafb6a7c05504be81e529f9ee4d
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-04-12 15:23:48 +0200

    Added sampling on the data.

--- download_plates.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-BEE="/Users/simondi/PROJECTS/target_infect_x_project/src/tix_mario/openBIS/Tools/BeeDataSetDownloader/BeeDataSetDownloader.sh"
-SAMPLER="/Users/simondi/PROJECTS/target_infect_x_project/src/tix_util/sample_plates.py"
-META="/Users/simondi/PHD/data/data/target_infect_x/experiment_meta_files/experiment_meta_file.tsv"
-
-for i in `python $SAMPLER $META`; do
- $BEE --user "simon.dirmeier@bsse.ethz.ch"   --password "@wesc4213"   --outputdir ~/PHD/data/data/target_infect_x/screening_data   --plateid $i --files ".*.mat" --verbose "6" --newest;
-done
