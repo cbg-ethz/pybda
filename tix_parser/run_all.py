a8ec90d0c4f414a57a760064442ef408670b20a0
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-04-19 09:48:47 +0200

    rm logging to file

--- tix_parser/run_all.py
+++ tix_parser/run_all.py
@@ -27,7 +27,6 @@ def parse_options(args):
 
 def main(args):
     c = Config(parse_options(args))
-    log_file = c.output_path + "/log.txt"
     logging.basicConfig(level=logging.INFO,
                         format='[%(levelname)-1s/%(processName)-1s/%(name)-1s]: %(message)s')
     # create plate parser object and parse the single plates
