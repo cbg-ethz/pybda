172cc2764adec37528e105b492a80cdb5540b6be
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-06-12 23:29:44 +0200

    Some renaming and additions

--- tix_analysis/tix_scripts/tix_normalize.py
+++ tix_analysis/tix_scripts/tix_normalize.py
@@ -2,62 +2,32 @@
 
 import sys
 import pandas
+import os
+import numpy
 
-import matplotlib.pyplot
-from matplotlib.backends.backend_pdf import PdfPages
-from sklearn import manifold
+if os.path.isdir("/cluster/home/simondi/spark/"):
+    is_cluster = True
+else:
+    is_cluster = False
 
-import findspark
-from sparkhpc import sparkjob
+if is_cluster:
+    file_name = "/cluster/home/simondi/simondi/tix/data/screening_data/cells_sample_10.tsv"
+else:
+    file_name = "/Users/simondi/PHD/data/data/target_infect_x/screening_data_subset/cells_sample_10.tsv"
 
-#spark_path = "/cluster/home/simondi/spark/"
-#findspark.init(spark_path)
+file_name= "/Users/simondi/PHD/data/data/target_infect_x/screening_data_subset/cells_sample_10_100lines.tsv"
 
-import pyspark
-from pyspark.sql.window import Window
-import pyspark.sql.functions as func
-from pyspark.rdd import reduce
-from pyspark.sql.types import DoubleType
-from pyspark.ml.feature import VectorAssembler, PCA
-from pyspark.ml.clustering import KMeans
-from pyspark.ml.linalg import SparseVector, VectorUDT, Vector, Vectors
+df = pandas.read_csv(file_name, sep="\t", header=0)
 
-# conf = pyspark.SparkConf().setAppName("app")
-# sc = pyspark.SparkContext(conf=conf)
-
-#sj = sparkjob.sparkjob(ncores=4)
-#sj.wait_to_start()
-#sc = sj.start_spark()
-# spark = pyspark.sql.SparkSession(sc)
-.builder \
-    .appName("PythonPi") \
-    .getOrCreate()
-
-
-file_name = "/cluster/home/simondi/simondi/tix/data/screening_data/cells_sample_10.tsv"
-
-df = spark.read.csv(path=file_name, sep="\t", header='true')
-df.cache()
-old_cols = df.schema.names
+old_cols = df.columns
 new_cols = list(map(lambda x: x.replace(".", "_"), old_cols))
-
-df = reduce(
-  lambda data, idx: data.withColumnRenamed(old_cols[idx], new_cols[idx]),
-  range(len(new_cols)), df)
-for i, x in enumerate(new_cols):
-    if x.startswith("cells"):
-        df = df.withColumn(x, df[x].cast("double"))
-
+df.columns = new_cols
 
 def z_score_w(col, w):
     avg = func.avg(col).over(w)
     sd = func.stddev(col).over(w)
     return (col - avg) / sd
 
-
-w = Window() \
-    .partitionBy(["study", "pathogen", "plate", "replicate"]) \
-    .rowsBetween(-sys.maxsize, sys.maxsize)
 
 for x in df.columns:
     if x.startswith("cells"):
