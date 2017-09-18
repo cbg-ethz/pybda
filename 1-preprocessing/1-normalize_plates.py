30564b9d0d39b6bc4f338055df28869b6573276e
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-09-18 15:02:35 +0200

    Removes tix normalize pandas

--- 1-preprocessing/1-normalize_plates.py
+++ 1-preprocessing/1-normalize_plates.py
@@ -1,17 +1,40 @@
 #!/usr/bin/env python
 
+
+import os
 import click
+import logging
+import findspark
+from sparkhpc import sparkjob
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 
 @click.command()
-@click.argument("file", type=str)
-def run(file):
+@click.argument("file",
+                type=str,
+                help="A tsv-file generated from `rnai-parse parse`")
+@click.argument("spark",
+                type=str,
+                help="Folder where you Spark installation lies.")
+def run(file, spark):
     """
-    Compute screen sets and their feature overlaps.
+    Do a normalization on plates.
     """
-    sets = _feature_sets(file)
-    _max_set(sets)
 
+    findspark.init(spark)
+    import pyspark
+
+    conf = pyspark.SparkConf().setAppName("normalize")
+    sc = pyspark.SparkContext(conf=conf)
+    spark = pyspark.sql.SparkSession(sc)
+    normalize(spark, file)
+
+
+def normalize(spark, file_name):
+    df = spark.read.csv(path=file_name, sep="\t", header='true')
+    df.groupby("")
 
 if __name__ == '__main__':
     run()
