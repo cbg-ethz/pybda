73bd890d2123458114be6c3a53d24e08c6a515ce
dirmeier <simon.dirmeier@web.de>
2017-10-07 20:38:42 +0200

    Added fit all and some mods

---
+++ 2-analysis/1c-kmeans-fit-all.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+if [ $# -ne 1 ]
+then
+  echo -e "Usage: ./$0 spark:master"
+  exit
+fi
+
+
+for i in {15..2};
+do
+  /cluster/home/simondi/spark/bin/spark-submit  --master $1 --num-executors 10 --executor-cores 1  --total-executor-cores 10 1-kmeans_spark.py -o /cluster/home/simondi/simondi/results/kmeans_all_no_vac_100 -f /cluster/work/bewi/members/simondi/data/tix/query_data/all_no_vac_100_cells.tsv  fit -k ${i}
+done
