f1e59093a0ae1c63afcd612542ba52b1c28fdf6b
dirmeier <simon.dirmeier@web.de>
2017-10-09 10:32:28 +0200

    Updates paths in kmeans methods

--- 2-analysis/1c-kmeans-fit-all.sh
+++ 2-analysis/1c-kmeans-fit-all.sh
@@ -8,7 +8,7 @@ then
 fi
 
 
-for i in {15..2};
+for i in {16..30};
 do
   /cluster/home/simondi/spark/bin/spark-submit  --master $1 --num-executors 10 --executor-cores 1  --total-executor-cores 10 1-kmeans_spark.py -o /cluster/home/simondi/simondi/results/kmeans_all_no_vac_100 -f /cluster/work/bewi/members/simondi/data/tix/query_data/all_no_vac_100_cells.tsv  fit -k ${i}
 done
