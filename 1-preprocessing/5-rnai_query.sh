997596842b4d2ccb5f222ce3a9c49d3e44d226c8
dirmeier <simon.dirmeier@web.de>
2017-11-17 16:14:48 +0100

    minor fixes to 5-query

--- 1-preprocessing/5-rnai_query.sh
+++ 1-preprocessing/5-rnai_query.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-  echo -e "Usage: ./$0 NUM_CELLS FEATURE_DB_FILE"
+  echo -e "Usage: $0 NUM_CELLS FEATURE_DB_FILE"
   exit
 fi
 
-echo -o "~/bsub/query_optimal_${1}_${2}.log" -e "~/bsub/query_optimal_${1}_${2}.err" \
-  -q normal.120h -M 100000 -n1 -R "rusage[mem=100000]"
-  rnai-query query \
-  --db "/cluster/home/simondi/simondi/data/tix/database/tix_index.db" \
-  --from-file "/cluster/home/simondi/PROJECTS/tix-util/results/1-preprocessing/0-features/${2}.tsv" \
-  --sample "${1}" \
-   "/cluster/home/simondi/simondi/data/tix/query_data/all_optimal_from_file_${2}_cells_${1}.tsv"
+reg=".+/(.+).tsv"
+[[ $2 =~ $reg ]]
+filename="${BASH_REMATCH[1]}"
+
+bsub -o "/cluster/home/simondi/bsub/query_optimal_${1}_${filename}.log" -e "/cluster/home/simondi/bsub/query_optimal_${1}_${filename}.err" -q normal.24h -M 100000 -n1 -R "rusage[mem=100000]" rnai-query query --db /cluster/work/bewi/members/simondi/data/tix/database/tix_index.db --from-file "${2}" --sample "${1}" "/cluster/home/simondi/simondi/data/tix/query_data/all_optimal_from_file_${filename}_cells_${1}.tsv"
