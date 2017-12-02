f982409bf3ca20582d19110a6c1ea5a375e3a78d
dirmeier <simon.dirmeier@web.de>
2017-12-02 01:16:10 +0100

    Minor fixes

--- 2-analysis/1c-kmeans-fit-all.sh
+++ 2-analysis/1c-kmeans-fit-all.sh
@@ -8,7 +8,7 @@ then
 fi
 
 
-for i in {10..50};
+for i in 1000 5000 10000;
 do
   /cluster/home/simondi/spark/bin/spark-submit  --master $1 --num-executors 10 --executor-cores 1  --total-executor-cores 10 1-kmeans_spark.py -o /cluster/home/simondi/simondi/results/kmeans_final_data -f /cluster/work/bewi/members/simondi/data/tix/query_data/all_optimal_from_file_feature_dbq_250_cells_100.tsv fit -k ${i}
 done
