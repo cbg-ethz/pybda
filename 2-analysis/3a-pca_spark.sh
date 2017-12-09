f51922b3471cfd9f48161efd6a1fb3be41941a96
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-12-09 21:34:40 +0100

    Removes pca script since we already have clusters

--- 2-analysis/3a-pca_spark.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $# -ne 2 ]
-then
-  echo -e "Usage: ./$0 spark:master kmeans_transformed-folder"
-  exit
-fi
-
-/cluster/home/simondi/spark/bin/spark-submit  --master $1 --num-executors 10 --executor-cores 1  --total-executor-cores 10 2-pca_spark.py -f ${2}
