ca3fc57260503f892ed83a71948bdeabc0b1946d
dirmeier <simon.dirmeier@bsse.ethz.ch>
2017-12-17 01:18:18 +0100

    Updated dpcs

--- 3-clustering/1d-kmeans-statistics.sh
+++ 3-clustering/1d-kmeans-statistics.sh
@@ -6,4 +6,4 @@ then
   exit
 fi
 
-/cluster/home/simondi/spark/bin/spark-submit  --master $1 --num-executors 10 --executor-cores 1  --total-executor-cores 10 2-kmeans_statistics-spark.py -f $2
+/cluster/home/simondi/spark/bin/spark-submit  --master $1 --num-executors 10 --executor-cores 1  --total-executor-cores 10 1-kmeans_statistics-spark.py -f $2
