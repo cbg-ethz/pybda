f74e042fab702bb58551d288f18c786afd390a4c
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-04-11 13:30:27 +0200

    Adds parquet sh

---
+++ 2-dimension_reduction/2a-parquet_to_tsv.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+if [ $# -ne 1 ]
+then
+  echo -e "Usage: ./$0 spark:master"
+  exit
+fi
+
+/cluster/home/simondi/spark/bin/spark-submit  \
+  --master $1  --num-executors 10 --executor-cores 1 \
+  --total-executor-cores 10 \
+  2-parquet_to_tsv.py \
+  -o /cluster/home/simondi/simondi/data/tix/2-fa-kmeans/kmeans_fa/all_optimal_from_file_feature_dbq_250_cells_100_sample.tsv   \
+  -f /cluster/home/simondi/simondi/data/tix/2-fa-kmeans/kmeans_fa/all_optimal_from_file_feature_dbq_250_cells_100
