434f34b70212847ab595befb3ea6641cb9954f3b
dirmeier <simon.dirmeier@web.de>
2018-04-30 17:10:35 +0200

    Resets paths in bash scrips

--- 2-dimension_reduction/2a-parquet_to_tsv.sh
+++ 2-dimension_reduction/2a-parquet_to_tsv.sh
@@ -6,9 +6,17 @@ then
   exit
 fi
 
+
+#/cluster/home/simondi/spark/bin/spark-submit  \
+#  --master $1  --num-executors 10 --executor-cores 1 \
+#  --total-executor-cores 10 \
+#  2-parquet_to_tsv.py \
+#  -f /cluster/home/simondi/simondi/data/tix/1-preprocessing/1-dimension_reduction/fa_10_cells/fa_parquet   \
+#  -o /cluster/home/simondi/simondi/data/tix/1-preprocessing/1-dimension_reduction/fa_10_cells/fa.tsv
+
 /cluster/home/simondi/spark/bin/spark-submit  \
   --master $1  --num-executors 10 --executor-cores 1 \
   --total-executor-cores 10 \
   2-parquet_to_tsv.py \
-  -o /cluster/home/simondi/simondi/data/tix/2-fa-kmeans/kmeans_fa/all_optimal_from_file_feature_dbq_250_cells_100_sample.tsv   \
-  -f /cluster/home/simondi/simondi/data/tix/2-fa-kmeans/kmeans_fa/all_optimal_from_file_feature_dbq_250_cells_100
+  -o /cluster/home/simondi/simondi/data/tix/1-preprocessing/1-dimension_reduction/1-factor_analysis/fa-all_optimal_from_file_feature_dbq_250_cells_100_sample.tsv   \
+  -f /cluster/home/simondi/simondi/data/tix/1-preprocessing/1-dimension_reduction/1-factor_analysis/fa-all_optimal_from_file_feature_dbq_250_cells_100
--- 2-dimension_reduction/3a-remove_outliers.sh
+++ 2-dimension_reduction/3a-remove_outliers.sh
@@ -7,9 +7,17 @@ then
   exit
 fi
 
+#/cluster/home/simondi/spark/bin/spark-submit  \
+#  --master $1  --num-executors 10 --executor-cores 1 \
+#  --total-executor-cores 10 \
+#  3-remove_outliers.py \
+#  -f /cluster/home/simondi/simondi/data/tix/1-preprocessing/1-dimension_reduction/fa_10_cells/fa_parquet \
+#  -o /cluster/home/simondi/simondi/data/tix/1-preprocessing/2-outlier_detection/out_10_cells
+
 /cluster/home/simondi/spark/bin/spark-submit  \
   --master $1  --num-executors 10 --executor-cores 1 \
   --total-executor-cores 10 \
   3-remove_outliers.py \
-  -f /cluster/home/simondi/simondi/results/kmeans_fa/all_optimal_from_file_feature_dbq_250_cells_100
-  -o /cluster/work/bewi/members/simondi/data/tix/query_data/all_optimal_from_file_feature_dbq_250_cells_100_removed_outloers
+  -f /cluster/home/simondi/simondi/data/tix/1-preprocessing/1-dimension_reduction/1-factor_analysis/fa-all_optimal_from_file_feature_dbq_250_cells_100 \
+  -o /cluster/home/simondi/simondi/data/tix/1-preprocessing/2-outlier_detection/1-mahalanobis/outlier_detection-all_optimal_from_file_feature_dbq_250_cells_100
+
