53c1a041c835ffb1cbab468b1e69aa46f8852ae9
dirmeier <simon.dirmeier@web.de>
2018-05-08 16:19:40 +0200

    Some file updates

--- 2-dimension_reduction/3a-remove_outliers.sh
+++ 2-dimension_reduction/3a-remove_outliers.sh
@@ -7,17 +7,10 @@ then
   exit
 fi
 
-#/cluster/home/simondi/spark/bin/spark-submit  \
-#  --master $1  --num-executors 10 --executor-cores 1 \
-#  --total-executor-cores 10 \
-#  3-remove_outliers.py \
-#  -f /cluster/home/simondi/simondi/data/tix/1-preprocessing/1-dimension_reduction/fa_10_cells/fa_parquet \
-#  -o /cluster/home/simondi/simondi/data/tix/1-preprocessing/2-outlier_detection/out_10_cells
-
 /cluster/home/simondi/spark/bin/spark-submit  \
   --master $1  --num-executors 10 --executor-cores 1 \
   --total-executor-cores 10 \
   3-remove_outliers.py \
-  -f /cluster/home/simondi/simondi/data/tix/1-preprocessing/1-dimension_reduction/1-factor_analysis/fa-all_optimal_from_file_feature_dbq_250_cells_100 \
-  -o /cluster/home/simondi/simondi/data/tix/1-preprocessing/2-outlier_detection/1-mahalanobis/outlier_detection-all_optimal_from_file_feature_dbq_250_cells_100
+  -f /cluster/home/simondi/simondi/data/tix/2-analysis/fa-all_optimal_from_file_feature_dbq_250_cells_100 \
+  -o /cluster/home/simondi/simondi/data/tix/2-analysis/outlier_detection-all_optimal_from_file_feature_dbq_250_cells_100
 
