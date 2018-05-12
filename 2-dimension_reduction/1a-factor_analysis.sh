cd1d9c3e8fbb9a75b440d6a67843e998b61deb24
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-05-12 22:55:58 +0200

    Traversion to snake

--- 2-dimension_reduction/1a-factor_analysis.sh
+++ 2-dimension_reduction/1a-factor_analysis.sh
@@ -14,4 +14,3 @@ fi
   1-factor_analysis-spark.py \
   -o /cluster/home/simondi/simondi/data/tix/2-analysis/fa-all_optimal_from_file_feature_dbq_250_cells_100 \
   -f /cluster/work/bewi/members/simondi/data/tix/0-query_data/all_optimal_from_file_feature_dbq_250_cells_100.tsv
-
