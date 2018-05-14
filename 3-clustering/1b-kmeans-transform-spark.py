1c45d7ffcc88af3bb08694ebd643778dc821d42a
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-05-14 16:33:58 +0200

    Added cluster transform script

--- 3-clustering/1b-kmeans-transform-spark.py
+++ 3-clustering/1b-kmeans-transform-spark.py
@@ -35,16 +35,19 @@ def read_args(args):
                         metavar="output-folder")
     parser.add_argument('-f',
                         type=str,
-                        help='the file or filder you want to cluster, i.e. a file derived '
-                             'from rnai-query like '
-                             'cells_sample_10_normalized_cut_100.tsv or '
-                             'cells_sample_10_normalized_cut_100_factors. If it '
-                             'is a folder we assume it is a parquet.',
+                        help="the folder originally used for clustering. "
+                             "this is probaly a folder called 'outlier-detection or so'",
+                        required=True,
+                        metavar="input")
+    parser.add_argument('-c',
+                        type=str,
+                        help="the folder prefix used for clustering. "
+                             "this is probaly a folder called 'kmeans-fit'",
                         required=True,
                         metavar="input")
     opts = parser.parse_args(args)
 
-    return opts.f, opts.o, opts
+    return opts.f, opts.o, opts.c, opts
 
 
 def data_path(file_name):
@@ -64,6 +67,16 @@ def write_parquet_data(file_name, data):
 def write_tsv_data(file_name, data):
     logger.info("Writing tsv: {}".format(file_name))
     data.write.csv(file_name, mode="overwrite", header=True, sep="\t")
+
+
+def k_fit_folders(clusterprefix):
+    mpaths = []
+    mreg =  re.compile(".*K\d+$")
+    for x in glob.glob(clusterprefix + "*"):
+         if pathlib.Path(x).is_dir():
+             if mreg.match(x):
+                 mpaths.append(x)
+    return mpaths
 
 
 def get_feature_columns(data):
@@ -108,60 +121,48 @@ def get_frame(file_name):
 
 
 def get_kmean_fit_statistics(mpaths, data):
+    logger.info("\tcomputing fit statistics using BIC")
     kmean_fits = []
     for mpath in mpaths:
         try:
-            # parameteres
+            # Get number of clusters
             K = int(re.match(".*_K(\d+)$", mpath).group(1))
-            logger.info("Loading model for K={}".format(K))
+            logger.info("\tloading model for K={}".format(K))
             model = KMeansModel.load(mpath)
             rss = model.computeCost(data)
+            # number of samples
             N = data.count()
-            P = 10
-            bic_3 = rss + numpy.log(N) * K * P
-            bic_2 =  N + N * numpy.log(2 * numpy.pi) + N * numpy.log(rss/N) + numpy.log(N) * (K + 1)
-            bic = N * numpy.log(rss / N) + K * numpy.log(N)
-
-            kmean_fits.append((K, model, bic, bic_2, bic_3))
-
+            # number of predictors (feature dimensionality)
+            P = len(numpy.asarray(df.select("features").take(1)).flatten())
+            bic = rss + numpy.log(N) * K * P
+            kmean_fits.append({"K": K, "model": model, "BIC": bic})
         except AttributeError as e:
             logger.error(
-              "Could not load model {}, due to: {}".format(mpath, str(e)))
-    kmean_fits.sort(key=lambda x: x[0])
-    print(kmean_fits)
+                "\tcould not load model {}, due to: {}".format(mpath, str(e)))
+    kmean_fits.sort(key=lambda x: x["K"])
+
     return kmean_fits
 
 
-def plot_cluster(file_name, outpath):
-    logger.info("Plotting cluster for: {}".format(file_name))
-    data = get_frame(file_name)
-    mpaths = [x for x in
-              glob.glob(model_path(outpath, file_name) + "*K*") if
-              pathlib.Path(x).is_dir()]
+def plot_cluster(outpath, kmean_fits):
+    plotpath = outpath + "-bic"
+    logger.info("Plotting cluster BICs to: {}".format(plotpath))
 
-    logger.info("Mpath: {}".format("\n".join(mpaths)))
-    logger.info("Models path:  {}".format(model_path(outpath, file_name)))
+    kmean_fits.sort(key=lambda x: x["K"])
+    ks  = [ x["K"] for x in kmean_fits ]
+    bic = [ x["BIC"] for x in kmean_fits ]
 
-    kmean_fits = get_kmean_fit_statistics(mpaths, data)
-
-    ks = [x[0] for x in kmean_fits]
-    bic = [x[2] for x in kmean_fits]
-    plot(ks, mses, "BIC", outpath, file_name)
-    bic2 = [x[3] for x in kmean_fits]
-    plot(ks, aics, "BIC 2", outpath, file_name)
-    bic3 = [x[4] for x in kmean_fits]
-    plot(ks, bics, "BIC 3", outpath, file_name)
-
+    statistics_file = plotpath + ".tsv"
+    logger.info("\tsaving bic tsv to: {}".format(statistics_file))
+    pandas.DataFrame(data={ "index": ks, "stat": bic }) \
+        .to_csv(statistics_file, sep="\t", index=0)
+        
+    plot(ks, bic, "BIC", plotpath)
 
 def plot(ks, score, axis_label, outpath, file_name):
-    plotfile = k_performance_plot_path(outpath, file_name, axis_label)
 
-    statistics_file = plotfile.replace("eps", "tsv")
-    pandas.DataFrame(data={ "index": ks, "stat": score }).to_csv(statistics_file, sep="\t", index=0)
-
-    font = {'weight': 'normal',
-            'family': 'sans-serif',
-            'size': 14}
+    plotfile = outpath + ".eps"
+    font = {'weight': 'normal', 'family': 'sans-serif', 'size': 14}
     plt.rc('font', **font)
     plt.figure()
     ax = plt.subplot(111)
@@ -178,65 +179,71 @@ def plot(ks, score, axis_label, outpath, file_name):
     plt.ylabel(axis_label, fontsize=15)
     plt.title('')
     ax.grid(True)
-    logger.info("Saving plot to: {}".format(plotfile))
+    logger.info("\tsaving plot to: {}".format(plotfile))
     plt.savefig(plotfile, bbox_inches="tight")
 
 
+def get_optimal_k(datafolder, clusterprefix):
+    logger.info("Finding optimal K for transformation...")
+
+    data = get_frame(datafolder)
+    mpaths = k_fit_folders(clusterprefix)
+    kmeans_fits = get_kmean_fit_statistics(mpaths, data)
+
+    plot_cluster(outpath)
 
 
 
-def transform_cluster(file_name, outpath):
-    bic =  get_optimal_k()
+def transform_cluster(datafolder, outpath, clusterprefix):
+    bic =  get_optimal_k(datafolder, clusterprefix)
+    # cpath = data_path(file_name)
+    # mpath = k_model_path(outpath, file_name, k)
+    # if not pathlib.Path(cpath).is_dir():
+    #     logger.error("Directory doesnt exist: {}".format(cpath))
+    #     return
+    # if not pathlib.Path(mpath).is_dir():
+    #     logger.error("Directory doesnt exist: {}".format(mpath))
+    #     return
+    #
+    # logger.info("Loading data: {}".format(cpath))
+    # logger.info("Loading model: {}".format(mpath))
+    # logger.info("Loading/clustering KMeansModel with k={}".format(k))
+    # data = read_parquet_data(cpath)
+    # model = KMeansModel.load(mpath)
+    #
+    # copath = k_transform_centers_path(outpath, file_name, k)
+    # logger.info("Writing cluster centers to: {}".format(copath))
+    # with open(copath, "w") as fh:
+    #     fh.write("#Clustercenters\n")
+    #     for center in model.clusterCenters():
+    #         fh.write("\t".join(map(str, center)) + '\n')
+    #
+    # # transform data and select
+    # data = model.transform(data).select(
+    #   "study", "pathogen", "library", "design", "replicate",
+    #   "plate", "well", "gene", "sirna", "well_type",
+    #   "image_idx", "object_idx", "prediction", "features")
+    # logger.info("Writing clustered data to parquet")
+    #
+    # opath = k_transform_path(outpath, file_name, k)
+    # write_parquet_data(opath, data)
 
 
-    cpath = data_path(file_name)
-    mpath = k_model_path(outpath, file_name, k)
-    if not pathlib.Path(cpath).is_dir():
-        logger.error("Directory doesnt exist: {}".format(cpath))
-        return
-    if not pathlib.Path(mpath).is_dir():
-        logger.error("Directory doesnt exist: {}".format(mpath))
-        return
-
-    logger.info("Loading data: {}".format(cpath))
-    logger.info("Loading model: {}".format(mpath))
-    logger.info("Loading/clustering KMeansModel with k={}".format(k))
-    data = read_parquet_data(cpath)
-    model = KMeansModel.load(mpath)
-
-    copath = k_transform_centers_path(outpath, file_name, k)
-    logger.info("Writing cluster centers to: {}".format(copath))
-    with open(copath, "w") as fh:
-        fh.write("#Clustercenters\n")
-        for center in model.clusterCenters():
-            fh.write("\t".join(map(str, center)) + '\n')
-
-    # transform data and select
-    data = model.transform(data).select(
-      "study", "pathogen", "library", "design", "replicate",
-      "plate", "well", "gene", "sirna", "well_type",
-      "image_idx", "object_idx", "prediction", "features")
-    logger.info("Writing clustered data to parquet")
-
-    opath = k_transform_path(outpath, file_name, k)
-    write_parquet_data(opath, data)
-
-
-def loggername(outpath, file_name):
+def loggername(outpath):
     return outpath + ".log"
 
 
 def run():
     # check files
-    file_name, outpath, opts = read_args(sys.argv[1:])
+    datafolder, outpath, clusterprefix, opts = read_args(sys.argv[1:])
 
     # logging format
     hdlr = logging.FileHandler(loggername(outpath))
     hdlr.setFormatter(frmtr)
     logger.addHandler(hdlr)
 
-    if not pathlib.Path(file_name).exists():
-        logger.error("Please provide a file: " + file_name)
+    if not pathlib.Path(datafolder).exists():
+        logger.error("Please provide a file: " + datafolder)
         return
 
     logger.info("Starting Spark context")
@@ -249,7 +256,7 @@ def run():
     spark = pyspark.sql.SparkSession(sc)
 
 
-    transform_cluster(file_name, outpath)
+    transform_cluster(datafolder, outpath, clusterprefix)
 
     logger.info("Stopping Spark context")
     spark.stop()
--- 3-clustering/1c-kmeans-transform.sh
+++ 3-clustering/1c-kmeans-transform.sh
@@ -6,4 +6,8 @@ then
   exit
 fi
 
-/cluster/home/simondi/spark/bin/spark-submit  --master $1 --num-executors 10 --executor-cores 1  --total-executor-cores 10 2-kmeans-spark.py -o /cluster/home/simondi/simondi/results/kmeans_fa -f /cluster/work/bewi/members/simondi/results/kmeans_fa/all_optimal_from_file_feature_dbq_250_cells_100 transform -k ${2}
+/cluster/home/simondi/spark/bin/spark-submit  --master $1 \
+     --num-executors 10 --executor-cores 1  \
+      --total-executor-cores 10 2-kmeans-spark.py \
+      -o /cluster/home/simondi/simondi/results/kmeans_fa \
+      -f /cluster/work/bewi/members/simondi/results/kmeans_fa/all_optimal_from_file_feature_dbq_250_cells_100 transform -k ${2}
