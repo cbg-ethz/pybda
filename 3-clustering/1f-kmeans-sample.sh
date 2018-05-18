b7eb100f2b876c42979fafb855ba2f44094051d3
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-05-18 14:51:38 +0200

    Removes sampling bash script

--- 3-clustering/1f-kmeans-sample.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $# -ne 3 ]
-then
-  echo -e "Usage: $0 spark:master kmeans_transformed_folder gene1,gene2,gene3"
-  exit
-fi
-
-/cluster/home/simondi/spark/bin/spark-submit  --master $1 --num-executors 10 --executor-cores 1  --total-executor-cores 10 3-sample_spark.py -f $2 -g $3
