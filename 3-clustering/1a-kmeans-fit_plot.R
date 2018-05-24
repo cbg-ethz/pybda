e790590ff09a4a145bfff8a5a4435b536901c3a9
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-05-24 22:20:13 +0200

    Adds in plotting

--- 3-clustering/1a-kmeans-fit_plot.R
+++ 3-clustering/1a-kmeans-fit_plot.R
@@ -3,6 +3,7 @@
 suppressPackageStartupMessages(library(argparse))
 suppressPackageStartupMessages(library(stringr))
 suppressPackageStartupMessages(library(readr))
+suppressPackageStartupMessages(library(dplyr))
 suppressPackageStartupMessages(library(ggplot2))
 suppressPackageStartupMessages(library(colorspace))
 suppressPackageStartupMessages(library(hrbrthemes))
@@ -16,33 +17,46 @@ suppressMessages(hrbrthemes::import_roboto_condensed())
 logr <- "logger"
 flog.logger(logr, futile.logger::INFO)
 
-plot.cluster.sizes <- function(plot.folder)
-{
-  plot.folder <- ""
-  pls <- list.files(plot.folder,  pattern="cluster_?[s|S]izes.tsv", full.names=TRUE)
-  dat <- purrr::map_dfr(pls, function(e) {
-    # parse the number of clusters from the file name
-    fl.suf <- as.integer(str_match(string=e, pattern=".*K(\\d+).*tsv")[2])
-    tab    <- readr::read_tsv(e, col_names="K", col_types="i")
-    tab$ClusterCount <- fl.suf
-    tab
-  })
 
-  ggplot(dat, aes(x = dat$K, y = factor(dat$ClusterCount), fill=factor(dat$ClusterCount))) +
-    geom_density_ridges(stat = "binline", scale = .4, draw_baseline = FALSE, bins=100, alpha=.5,
-    ) +
+plot.cluster.sizes <- function(dat, plot.folder)
+{
+  flog.info('Plotting cluster sizes', name=logr)
+
+  dat$ClusterCount <- factor(dat$ClusterCount, levels=rev(sort(unique(dat$ClusterCount))))
+
+
+  plt <- ggplot(dat, aes(x = dat$K, y = dat$ClusterCount, fill = dat$ClusterCount)) +
+    geom_density_ridges(stat = "binline", scale = .4, draw_baseline = FALSE, bins=100, alpha=.5) +
     theme_ridges() +
     hrbrthemes::theme_ipsum() +
     colorspace::scale_fill_discrete_sequential("Blues", c1 = 20, c2 = 70, l1 = 25, l2 = 100) +
     scale_y_discrete("# clusters") +
     scale_x_log10("# cells per cluster") +
-    guides(fill=FALSE)
+    guides(fill=FALSE) +
+    theme(axis.title.x = element_text(size=20),
+          axis.title.y = element_text(size=20),
+          axis.text.x = element_text(size=15),
+          axis.text.y = element_text(size=15))
+
+  for (i in c("svg", "png", "eps")) {
+      ggsave(plt,
+            filename=paste0(plot.folder,"/kmeans-fit-clustersize_histogram.", i),
+             width=10, height=7)
+  }
 
 }
 
 
-(run <- function()
+plot.cluster.stats <- function(dat, plot.folder)
 {
+  fl.out <- paste0(plot.folder,"/kmeans-fit-clustersize_stats.tsv")
+  cl <-  group_by(dat, ClusterCount) %>%
+    dplyr::summarize(n=paste(sprintf("%.2f", quantile(K)/max(K)), collapse=", "))
+  readr::write_tsv(x=cl,  path=fl.out.)
+}
+
+
+(run <- function() {
   parser <- ArgumentParser()
   parser$add_argument("-f", "--folder", help = "folder where the cluster size files lie")
 
@@ -52,8 +66,21 @@ plot.cluster.sizes <- function(plot.folder)
     stop(parser$print_help())
   }
 
-  lg.file <- paste0(dir, "/kmeans-fit-plot.log")
   flog.appender(appender.file(lg.file), name=logr)
 
-  plot.cluster.sizes(opt$folder)
+  plot.folder <- opt$folder
+  lg.file <- paste0(plot.folder, "/kmeans-fit-plot.log")
+
+  pls <- list.files(plot.folder,  pattern="cluster_?[s|S]izes.tsv", full.names=TRUE)
+  dat <- purrr::map_dfr(pls, function(e) {
+    # parse the number of clusters from the file name
+    fl.suf <- as.integer(str_match(string=e, pattern=".*K(\\d+).*tsv")[2])
+    tab    <- readr::read_tsv(e, col_names="K", col_types="i")
+    tab$ClusterCount <- fl.suf
+    tab
+  })
+
+  plot.cluster.sizes(dat, plot.folder)
+  plot.cluster.stats(dat, plot.folder)
+
 })()
