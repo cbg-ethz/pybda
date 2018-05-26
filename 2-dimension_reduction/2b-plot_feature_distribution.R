07f2f8b0d03d03a3bb2d4ccadfb21dbcf24d8f24
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-05-26 15:30:04 +0200

    Updates FA distribution plotting

--- 2-dimension_reduction/2b-plot_feature_distribution.R
+++ 2-dimension_reduction/2b-plot_feature_distribution.R
@@ -17,11 +17,12 @@ options(stringsAsFactors=FALSE)
 plot.distributions <- function(out.dir, data.file)
 {
   fr           <- data.table::fread(data.file, sep="\t")
-  colnames(fr) <- paste0("factor_", seq(ncol(fr)))
+  colnames(fr) <- paste0("Factor ", seq(ncol(fr)))
   cols         <- colnames(fr)
   if (!dir.exists(out.dir)) dir.create(out.dir)
 
-  for (col in cols) {
+  for (col in cols)
+  {
     cl <- fr[ ,get(col)]
     cl.var  <- var(cl, na.rm=T)
     cl.mean <- mean(cl, na.rm=TRUE)
@@ -29,24 +30,31 @@ plot.distributions <- function(out.dir, data.file)
 
     pl <- ggplot(fr[cl.no.out], aes(x=fr[cl.no.out ,get(col)])) +
       hrbrthemes::theme_ipsum_rc(base_family="Helvetica") +
-      scale_x_continuous(paste("Feature:", col)) +
+      scale_x_continuous(paste(col)) +
       scale_y_continuous("Count") +
       geom_histogram(bins=100, fill="darkgrey") +
       theme(panel.grid.major=element_blank(),
-            axis.title.y = element_text(size=12),
-            axis.title.x = element_text(size=12))
+            axis.title.y = element_text(size=20),
+            axis.title.x = element_text(size=20),
+            axis.text.x = element_text(size=15),
+            axis.text.y = element_text(size=15),
+            panel.grid.minor = element_blank(),
+            axis.line = element_line(color="black", size = .75))
 
-    for (form in c("eps", "png", "svg")) {
-        ggsave(plot=pl, paste0(out.dir ,"/feature_", col, ".", form),
-               dpi=720, width=10, height=10  )
+    fl.o <- tolower(sub(" ", "_" , col))
+    for (form in c("eps", "png", "svg"))
+    {
+        ggsave(plot=pl, paste0(out.dir ,"/feature_", fl.o, ".", form),
+               dpi=720, width=8, height=10)
     }
   }
+
 }
 
 (run <- function()
 {
   parser <- ArgumentParser()
-  parser$add_argument("infile", help = "Folder in which fa is", type="character")
+  parser$add_argument("infile", help = "Name of subsampled fa entries (e.g. 'fa-sample.tsv')", type="character")
   opt <- parser$parse_args()
   fl <- opt$infile
   dir <- stringr:::str_match("(.*).tsv", string=fl)[2]
