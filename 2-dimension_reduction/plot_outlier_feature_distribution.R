c6423066d7afa5cc9956cd90e3eee10d425148b9
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-05-27 12:21:48 +0200

    Adds plotting of maha scattering

--- 2-dimension_reduction/plot_outlier_feature_distribution.R
+++ 2-dimension_reduction/plot_outlier_feature_distribution.R
@@ -14,41 +14,12 @@ suppressMessages(hrbrthemes::import_roboto_condensed())
 options(stringsAsFactors=FALSE)
 
 
-plot.distributions <- function(out.dir, fr)
+plot.distributions <- function(out.dir, data.file)
 {
-  for (col in cols)
-  {
-    cl <- fr[ ,get(col)]
-    cl.var  <- var(cl, na.rm=T)
-    cl.mean <- mean(cl, na.rm=TRUE)
-    cl.no.out <- which(abs(fr[ ,get(col)]) <= cl.mean + 4 * cl.var)
-
-    pl <- ggplot(fr[cl.no.out], aes(x=fr[cl.no.out ,get(col)])) +
-      hrbrthemes::theme_ipsum_rc(base_family="Helvetica") +
-      scale_x_continuous(paste(col)) +
-      scale_y_continuous("Count") +
-      geom_histogram(bins=100, fill="darkgrey") +
-      theme(panel.grid.major=element_blank(),
-            axis.title.y = element_text(size=20),
-            axis.title.x = element_text(size=20),
-            axis.text.x = element_text(size=15),
-            axis.text.y = element_text(size=15),
-            panel.grid.minor = element_blank(),
-            axis.line = element_line(color="black", size = .75))
-
-    fl.o <- tolower(sub(" ", "_" , col))
-    for (form in c("eps", "png", "svg"))
-    {
-        ggsave(plot=pl, paste0(out.dir ,"/feature_", fl.o, ".", form),
-               dpi=720, width=8, height=10)
-    }
-  }
-
-}
-
-
-scatter.distributions <- function(out.dir, fr)
-{
+  fr           <- data.table::fread(data.file, sep="\t")
+  colnames(fr) <- paste0("Factor ", seq(ncol(fr)))
+  cols         <- colnames(fr)
+  if (!dir.exists(out.dir)) dir.create(out.dir)
 
   fr <- as.matrix(fr)
   fr.maha <- stats::mahalanobis(fr, colMeans(fr), cov(fr))
@@ -58,25 +29,24 @@ scatter.distributions <- function(out.dir, fr)
   frs <- fr.flt[seq(min(nrow(fr.flt), 10000)), ]
   frs <- as.data.table(frs)
 
-
-  frs <- fr[seq(min(nrow(fr), 10000)), ]
   plt <- ggplot(frs) +
     geom_point(aes(frs[,get("Factor 1")], frs[,get("Factor 2")]), size=.5) +
     hrbrthemes::theme_ipsum_rc(base_family="Helvetica") +
-    scale_x_continuous("Factor 1") +
-    scale_y_continuous("Factor 2") +
+    scale_x_continuous("Factor 1", limits=c(-5, 5)) +
+    scale_y_continuous("Factor 2", limits=c(-5, 5)) +
     theme(axis.title.y = element_text(size=20),
           axis.title.x = element_text(size=20),
           axis.text.x = element_text(size=15),
           axis.text.y = element_text(size=15),
           panel.grid.minor = element_blank())
+
   for (form in c("eps", "png", "svg"))
   {
       ggsave(plot=plt, paste0(out.dir ,"/feature_scatter_plot.", form),
              dpi=720, width=8, height=10)
   }
-}
 
+}
 
 (run <- function()
 {
@@ -87,11 +57,5 @@ scatter.distributions <- function(out.dir, fr)
   dir <- stringr:::str_match("(.*).tsv", string=fl)[2]
   out.dir   <- paste0(dir, "-feature_distributions/")
 
-  fr           <- data.table::fread(fl, sep="\t")
-  colnames(fr) <- paste0("Factor ", seq(ncol(fr)))
-  cols         <- colnames(fr)
-  if (!dir.exists(out.dir)) dir.create(out.dir)
-
-  plot.distributions(out.dir, fr)
-  scatter.distributions(out.dir, fr)
+  plot.distributions(out.dir, fl)
 })()
