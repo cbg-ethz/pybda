ca8e534da823e5896082150b00aa593235cd4e73
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-06-02 19:55:38 +0200

    Minor updates to plotting

--- 2-dimension_reduction/1b-factor_analysis_plot.R
+++ 2-dimension_reduction/1b-factor_analysis_plot.R
@@ -21,36 +21,37 @@ plot.likelihood <- function(plotout, likelhood.file)
   full.tbl <- readr::read_tsv(likelhood.file, col_names=FALSE) %>%
     as.tbl %>%
     dplyr::mutate(Iteration=0:(n()-1)) %>%
-    as.data.frame
-  full.tbl$X1 <- full.tbl$X1
+    as.data.frame()
 
   lims <- c(4.8e08, 6.9e08, 8.9e08, 1.1e09, 1.3e09)
   plt <-
     ggplot2::ggplot(full.tbl, aes(x=Iteration, y=X1)) +
-    ggplot2::geom_line(size=1.5) +
+    ggplot2::geom_line(size=1.25) +
     xlab("Iteration") +
     ylab(expression(paste("-\u2113(", theta, ")"))) +
     labs(title="Factor analysis likelihood path") +
-    hrbrthemes::theme_ipsum_rc() +
+    hrbrthemes::theme_ipsum() +
     scale_y_reverse(labels=rev(lims), breaks= lims) +
     ggplot2::theme(axis.text.x  = ggplot2::element_text( size=18),
                    axis.text.y  = ggplot2::element_text(size=18),
+                   axis.line = element_line(size = .5, linetype = "solid"),
+                   panel.grid.minor = element_blank(),
+                   panel.grid.major = element_blank(),
                    axis.title.x   = ggplot2::element_text(size=20),
                    axis.title.y   = ggplot2::element_text(size=20))
 
-  ggsave(paste0(plotout, ".png"), dpi=720, width=10, height=10)
+  ggsave(plot=plt, filename=paste0(plotout, ".", "png"), dpi=720, width=10, height=7)
 
 }
 
 
 plot.factors <- function(plotout, factors.file)
 {
-
   hrbrthemes::import_roboto_condensed()
   full.tbl <- readr::read_tsv(factors.file, col_names=TRUE) %>%
     as.data.frame() %>%
     as.matrix() %>%
-     t()
+    t()
 
   P <- nrow(full.tbl)
   C <- ncol(full.tbl)
@@ -61,18 +62,23 @@ plot.factors <- function(plotout, factors.file)
                   Cumulative=cumsum(vars.explained))
 
   plt <-
-    ggplot(X) + geom_bar(aes(x=Factors, y=Cumulative), stat="identity") +
-    geom_text(aes(x=Factors, y=Cumulative, label=sprintf("%0.2f", Cumulative)), vjust=1.1, stat="identity") +
+    ggplot(X) +
+    geom_bar(aes(x=Factors, y=Cumulative), stat="identity") +
+    geom_text(aes(x=Factors, y=Cumulative, label=sprintf("%0.2f", Cumulative)), vjust=-.25, stat="identity") +
     ylab("Cumulative variance") +
     labs(title="Factor loadings variance explained") +
-    hrbrthemes::theme_ipsum_rc() +
+    hrbrthemes::theme_ipsum() +
     ggplot2::theme(axis.text.x  = ggplot2::element_text( size=18),
                    axis.text.y  = ggplot2::element_text(size=18),
                    axis.title.x   = ggplot2::element_text(size=20),
                    axis.title.y   = ggplot2::element_text(size=20),
-                   panel.grid.major.x = element_line(colour = 'black', linetype = 'dotted'))
-  ggsave(paste(plotout, "variance_explained.png", sep="-"), dpi=720,
-         width=10, height=10)
+                   panel.grid.major.x = element_blank(),
+                   panel.grid.minor = element_blank())
+
+  for (i in c("png", "svg", "eps"))
+  {
+    ggsave(plot= plt, filename=paste0(plotout, "-variance_explained.", i), dpi=720, width=10, height=7)
+  }
 
   vars.explained <- apply(full.tbl, 2, function(e) sum(e**2) / P)
   idx <- sort(vars.explained, decreasing=TRUE, index.return=TRUE)$ix
@@ -85,17 +91,20 @@ plot.factors <- function(plotout, factors.file)
     geom_point(color = "red") +
     geom_text_repel( point.padding = NA, size=2.5, arrow=NULL, segment.size = 0, force=4) +
     ggplot2::geom_segment(aes(x=0, y=0, xend=Factor1, yend=Factor2),
-                          data=X, color=theme$palette$swatch[2], alpha=.5,
+                          data=X, alpha=.5,
                           linetype="dashed",
                           arrow = arrow(length = unit(0.25,"cm") )) +
-    hrbrthemes::theme_ipsum_rc() +
+    hrbrthemes::theme_ipsum() +
     ggplot2::theme(axis.text.x  = ggplot2::element_text( size=18),
                    axis.text.y  = ggplot2::element_text(size=18),
+                   panel.grid.minor = element_blank(),
                    axis.title.x   = ggplot2::element_text(size=20),
                    axis.title.y   = ggplot2::element_text(size=20))
 
-  ggsave(paste(plotout, "biplot.png", sep="-") , dpi=720, width=10, height=10)
-
+  for (i in c("png", "svg", "eps"))
+  {
+    ggsave(plot= plt, filename=paste0(plotout, "-biplot.", i), dpi=720, width=10, height=7)
+  }
 }
 
 
---
+++ 3-clustering/1c-gmm-statistics-spark.py
@@ -0,0 +1,149 @@
+import argparse
+import logging
+import sys
+import pandas
+import pathlib
+import pyspark
+import numpy
+import re
+import glob
+import scipy
+from scipy import spatial
+
+from functools import partial
+import multiprocessing as mp
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+frmtr = logging.Formatter(
+  '[%(levelname)-1s/%(processName)-1s/%(name)-1s]: %(message)s')
+
+spark = None
+
+
+def read_args(args):
+    parser = argparse.ArgumentParser(
+      description="Compute statistics of a clustered dataset.")
+    parser.add_argument('-f',
+                        type=str,
+                        help="the folder where the  data lie,"
+                             " i.e. 'kmeans-transformed'",
+                        required=True, metavar="input-folder")
+
+    opts = parser.parse_args(args)
+    return opts.f, opts
+
+
+def read_parquet_data(file_name):
+    logger.info("Reading parquet: {}".format(file_name))
+    return spark.read.parquet(file_name)
+
+
+def write_parquet_data(file_name, data):
+    logger.info("Writing parquet: {}".format(file_name))
+    data.write.parquet(file_name, mode="overwrite")
+
+
+def write_pandas_tsv(file_name, data):
+    data.to_csv(file_name, sep="\t", index=False)
+
+
+def count_statistics(data, folder, what):
+    dnts = data.groupby(what).count()
+    dnts = dnts.select(what + ["count"]).dropDuplicates()
+    outfolder = folder + "-statistics-" + "_".join(what) + "_counts"
+    logger.info("Writing sample table to: {}".format(outfolder))
+    #dnts.write.csv(path=outfile, sep="\t", header=True)
+    dnts.toPandas().to_csv(outfolder + ".tsv", sep="\t", index=False)
+
+
+def compute_silhouettes(outfolder):
+    out_silhouette = outfolder + "-statistics-silhouette.tsv"
+    logger.info("Writing silhouettes to: {}".format(out_silhouette))
+
+    files = [x for x in glob.glob(outfolder + "-clusters/*") if x.endswith(".tsv")]
+    K = len(files)
+    with open(out_silhouette, "w") as ot:
+        ot.write("#Cluster\tNeighbor\tSilhouette\n")
+        for i in range(K):
+            _compute_silhouette(files, i, K, ot)
+
+
+def read_matrix(fl):
+    return pandas.read_csv(fl, sep="\t", nrows=1000,
+                           usecols=lambda x: x.startswith("f_"))
+
+
+def _compute_silhouette(outfiles, i, K, ot):
+    np_i = read_matrix(outfiles[i])
+    min_cluster, min_distance = mp_min_distance(i, K, np_i, outfiles)
+    within_distance = _mean_distance(i, np_i, outfiles)
+    silhouette = (min_distance - within_distance) / numpy.maximum(min_distance,
+                                                                  within_distance)
+    for clust, sil in zip(min_cluster, silhouette):
+        ot.write("{}\t{}\t{}".format(i, clust, sil) + "\n")
+
+
+def mp_min_distance(i, K, np_i, outfiles):
+    n_cores = mp.cpu_count()
+    itr = numpy.array([j for j in range(K) if j != i])
+    distances = [_mean_distance(i, np_i, outfiles) for i in itr]
+    distances = numpy.vstack(distances).T
+    argmins = numpy.argmin(distances, axis=1)
+    min_distances = numpy.min(distances, axis=1)
+    arg = itr[argmins]
+    return arg, min_distances
+
+
+def _mean_distance(j, np, outfiles):
+    """Computes distances between np array and np_j"""
+    distance = 0
+    cnt = 0
+    np_j = read_matrix(outfiles[j])
+    distances = scipy.spatial.distance.cdist(np, np_j)
+    return numpy.mean(distances, axis=1)
+
+
+def statistics(outfolder):
+
+    logger.info("Loading Kmeans clustering")
+    data = read_parquet_data(outfolder)
+
+    # Group the data by some criteria dnd plot the statistics as parquet files
+    count_statistics(data, outfolder, ["gene", "prediction"])
+    count_statistics(data, outfolder, ["pathogen", "prediction"])
+    count_statistics(data, outfolder, ["gene", "pathogen", "prediction"])
+    compute_silhouettes(outfolder)
+
+
+def run():
+    # check files
+    folder, opts = read_args(sys.argv[1:])
+    if not pathlib.Path(folder).is_dir():
+        logger.error("Folder does not exist: " + folder)
+        return
+
+        # logging format
+    hdlr = logging.FileHandler(folder + "-statistics.log")
+    hdlr.setFormatter(frmtr)
+    logger.addHandler(hdlr)
+
+    logger.info("Starting Spark context")
+
+    # spark settings
+    pyspark.StorageLevel(True, True, False, False, 1)
+    conf = pyspark.SparkConf()
+    sc = pyspark.SparkContext(conf=conf)
+    global spark
+    spark = pyspark.sql.SparkSession(sc)
+    try:
+        statistics(folder)
+    except Exception as e:
+        logger.error("Random exception: {}".format(str(e)))
+
+    logger.info("Stopping Spark context")
+    spark.stop()
+
+
+if __name__ == "__main__":
+    run()
