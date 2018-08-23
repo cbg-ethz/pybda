1bb7c5fccea81704ba3366be2fb7e591b5eb0cfd
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-08-23 20:07:26 +0200

    Updates plotting of thingy

--- _util/parse_dag.py
+++ _util/parse_dag.py
@@ -8,26 +8,42 @@ edge_match = re.compile("^\s+(\d+)\s->\s(\d+)\s+$")
 label_match = re.compile("^\s+(\d+)\[label\s\=\s\"(.*?)\".*$")
 
 
-def process_line(line, nodes, edges):
-    if node_match.match(line):
+def process_line(line, base_nodes):
+    if "graph[" in line:
+        print("\tgraph[bgcolor=white, margin=0,fontname=\"Roboto\"];")
+    elif "node[" in line:
+        print("\tnode[shape=rectangle, "
+              "fontname=\"Roboto\", fontsize=10, penwidth=2];")
+    elif "edge[" in line:
+        print("\tedge[penwidth=1, color=grey80, arrowsize=.8];")
+        print("\tnodesep=1.5;")
+    elif node_match.match(line):
         match = label_match.match(line)
         idx, label = match.group(1), match.group(2)
-        nodes[idx] = label
+        base_nodes[label] = int(idx)
+        if int(idx) != 0:
+            print("\t" + idx + "[label = \"" + label +
+                  "\", shape=\"rectangle\"]")
     elif edge_match.match(line):
         match = edge_match.match(line)
         fr, to = match.group(1), match.group(2)
-        edges.append([fr, to])
+        if int(fr) == 1 and int(to) == 0:
+            all_node = str(1 + max(base_nodes.values()))
+            print("\t" + all_node + "[label = \"data\", shape=\"rectangle\"];")
+            for k in ["fa", "pca", "kpca"]:
+                print("\t" + all_node + " -> " + str(base_nodes[k]))
+        if int(to) != 0:
+            print("\t" + fr + " -> " + to)
+    else:
+        print(line.strip())
 
 
 @click.command()
 @click.argument("file", type=click.File('r'))
 def run(file):
-    nodes = {}
-    edges = []
+    base_nodes = {}
     for line in file:
-        process_line(line, nodes, edges)
-    for [fr, to] in edges:
-        print(nodes[fr], "\t", nodes[to])
+        process_line(line, base_nodes)
 
 
 if __name__ == "__main__":
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-
 echo "Installing dependencies ..."
 python install -r requirements.txt;
 echo "Finished installing"
