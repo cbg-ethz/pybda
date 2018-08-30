9f3d019da37cbf9d5494cb916028ac11893aae2e
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-08-30 22:37:29 +0200

    Adds FAFit

--- koios/factor_analysis.py
+++ koios/factor_analysis.py
@@ -20,16 +20,13 @@
 
 
 import logging
-
-import numpy
-import pandas
-import pyspark.sql.functions as func
 import scipy
+import pyspark.sql.functions as func
 from pyspark.ml.linalg import VectorUDT
 from pyspark.mllib.linalg.distributed import RowMatrix, DenseMatrix
 from pyspark.sql.functions import udf
-
 from koios.dimension_reduction import DimensionReduction
+from koios.util.factor_analysis_fit import FactorAnalysisFit
 from koios.util.stats import svd, column_statistics
 
 logger = logging.getLogger(__name__)
@@ -44,39 +41,37 @@ class FactorAnalysis(DimensionReduction):
     @staticmethod
     def _tilde(X, psi_sqrt, n_sqrt):
         norm = psi_sqrt * n_sqrt
-        Xtilde = RowMatrix(X.rows.map(lambda x: x / norm))
-        return Xtilde
+        return RowMatrix(X.rows.map(lambda x: x / norm))
 
     @staticmethod
     def _loglik(llconst, unexp_var, s, psi, n):
-        ll = llconst + numpy.sum(numpy.log(s))
-        ll += unexp_var + numpy.sum(numpy.log(psi))
+        ll = llconst + scipy.sum(scipy.log(s))
+        ll += unexp_var + scipy.sum(scipy.log(psi))
         ll *= -n / 2.
 
         return ll
 
     @staticmethod
     def _update_factors(s, V, sqrt_psi):
-        W = numpy.sqrt(numpy.maximum(s - 1., 0.))[:, numpy.newaxis] * V
+        W = scipy.sqrt(scipy.maximum(s - 1., 0.))[:, scipy.newaxis] * V
         W *= sqrt_psi
-
         return W
 
     def _update_variance(self, var, W):
-        psi = numpy.maximum(var - numpy.sum(W ** 2, axis=0), self.__threshold)
+        psi = scipy.maximum(var - scipy.sum(W ** 2, axis=0), self.__threshold)
         return psi
 
     def _estimate(self, X, var, n_factors):
         n, p = X.numRows(), X.numCols()
-        old_ll = -numpy.inf
-        llconst = p * numpy.log(2. * numpy.pi) + n_factors
-        psi = numpy.ones(p, dtype=numpy.float32)
-        nsqrt = numpy.sqrt(n)
+        old_ll = -scipy.inf
+        llconst = p * scipy.log(2. * scipy.pi) + n_factors
+        psi = scipy.ones(p, dtype=scipy.float32)
+        nsqrt = scipy.sqrt(n)
         logliks = []
 
         logger.info("\tcomputing factor analysis")
         for i in range(self.__max_iter):
-            sqrt_psi = numpy.sqrt(psi) + self.__threshold
+            sqrt_psi = scipy.sqrt(psi) + self.__threshold
             s, V, unexp_var = svd(self._tilde(X, sqrt_psi, nsqrt), n_factors)
             s = s ** 2
 
@@ -88,18 +83,18 @@ class FactorAnalysis(DimensionReduction):
             # variance update
             psi = self._update_variance(var, W)
 
-            if numpy.abs(ll - old_ll) < 0.001:
+            if scipy.abs(ll - old_ll) < 0.001:
                 break
             old_ll = ll
 
         return W, logliks, psi
 
     def _transform(self, X, W, psi):
-        logger.info("\ttransforming data")
-        Ih = numpy.eye(len(W))
+        logger.info("Transforming data")
+        Ih = scipy.eye(len(W))
         Wpsi = W / psi
-        cov_z = scipy.linalg.inv(Ih + numpy.dot(Wpsi, W.T))
-        tmp = numpy.dot(Wpsi.T, cov_z)
+        cov_z = scipy.linalg.inv(Ih + scipy.dot(Wpsi, W.T))
+        tmp = scipy.dot(Wpsi.T, cov_z)
         tmp_dense = DenseMatrix(
           numRows=tmp.shape[0], numCols=tmp.shape[1], values=tmp.flatten())
 
@@ -112,7 +107,6 @@ class FactorAnalysis(DimensionReduction):
 
         return X
 
-
     def fit(self, data, n_factors):
         logger.info("Running factor analysis ...")
         X = data.rdd
@@ -121,19 +115,16 @@ class FactorAnalysis(DimensionReduction):
         W, ll, psi = self._estimate(X, var, n_factors)
         X = self._transform(X, W, psi)
 
-        X = X.withColumn('row_index', func.monotonically_increasing_id())
-        data = data.withColumn('row_index', func.monotonically_increasing_id())
-        data = data.join(X["row_index", "features"],
-                         on=["row_index"]).drop("row_index")
+        data = self._join(data, X)
         del X
 
-        write_parquet_data(outpath, data)
+        return FactorAnalysisFit(data, W, psi, ll)
 
-        logger.info("\twriting factor to data")
-        W = pandas.DataFrame(data=W)
-        W.columns = features
-        W.to_csv(outpath + "_factors.tsv", sep="\t", index=False)
+    @staticmethod
+    def _join(data, X):
+        X = X.withColumn("row_index", func.monotonically_increasing_id())
+        data = data.withColumn("row_index", func.monotonically_increasing_id())
+        data = data.join(
+          X["row_index", "features"], on=["row_index"]).drop("row_index")
 
-        logger.info("\twriting likelihood profile")
-        L = pandas.DataFrame(data=ll)
-        L.to_csv(outpath + "_likelihood.tsv", sep="\t", index=False)
+        return data
