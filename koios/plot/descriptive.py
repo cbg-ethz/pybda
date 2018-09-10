2a91f380e8f2d92209592d67233ce2024963e27b
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-09-10 21:20:11 +0100

    Adds plotting of descriptive statistics

--- koios/plot/descriptive.py
+++ koios/plot/descriptive.py
@@ -20,19 +20,54 @@
 
 
 import logging
+import scipy
 import matplotlib.pyplot as plt
-import numpy
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 plt.style.use(["seaborn-whitegrid"])
 plt.rcParams['font.family'] = 'sans-serif'
-plt.rcParams['font.sans-serif'] = ['Verdana']
+plt.rcParams['font.sans-serif'] = ['Tahome']
 
 
-def scatter(data, xlab, ylab, file):
-    pass
+def scatter(file_name, x, y,
+            xlab, ylab, xlim=2, ylim=2,
+            xlabpos=.95, ylabpos=.885):
+    _, ax = plt.subplots(figsize=(8, 5), dpi=720)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(True)
+    ax.spines["left"].set_visible(True)
+    ax.xaxis.set_label_coords(x=xlabpos, y=-0.1)
+    ax.yaxis.set_label_coords(x=-0.05, y=ylabpos)
+    ax.grid(linestyle="")
 
-def histogram(data, xlab, ylab, file):
-    pass
+    plt.xlim(-xlim, xlim)
+    plt.ylim(-ylim, ylim)
+    plt.xticks(scipy.arange(-xlim, xlim + 1, step=1))
+    plt.yticks(scipy.arange(-ylim, ylim + 1, step=1))
+
+    plt.scatter(x, y, color="black", alpha=.5, s=.5)
+    plt.xlabel(xlab, fontsize=15)
+    plt.ylabel(ylab, fontsize=15)
+
+    plt.savefig(file_name, dpi=720)
+
+
+def histogram(file_name, x, xlab, ylab="Frequency",
+              xlabpos=.95, ylabpos=.885):
+    _, ax = plt.subplots(figsize=(8, 5), dpi=720)
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
+    ax.spines["bottom"].set_visible(True)
+    ax.spines["left"].set_visible(True)
+    ax.xaxis.set_label_coords(x=xlabpos, y=-0.1)
+    ax.yaxis.set_label_coords(x=-0.05, y=ylabpos)
+    ax.grid(linestyle="")
+
+    plt.hist(x, color="black", alpha=.5, bins=50)
+
+    plt.xlabel(xlab, fontsize=15)
+    plt.ylabel(ylab, fontsize=15)
+    plt.savefig(file_name, dpi=720)
