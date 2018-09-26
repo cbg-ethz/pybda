807fb5151cd894407f12f5742f4c7bcdcf8367a7
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-09-26 21:58:37 +0200

    Implements cluster silhouettes

--- koios/cluster_statistics.py
+++ koios/cluster_statistics.py
@@ -17,9 +17,18 @@
 #
 # @author = 'Simon Dirmeier'
 # @email = 'simon.dirmeier@bsse.ethz.ch'
-
-
+import glob
 import logging
+
+import click
+import scipy
+import time
+
+import numpy
+import pandas
+
+from koios.io.file import find_by_suffix
+from koios.util.stats import sample
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -29,79 +38,61 @@ class ClusterStatistics:
     def __init__(self, spark):
         self.__spark = spark
 
-    @staticmethod
-    def count_statistics(data, what):
+    def count_statistics(self, data, what):
         return (data
                 .groupby(what)
                 .count()
                 .select(what + ["count"])
                 .dropDuplicates())
 
+    @staticmethod
+    def _read_matrices(files):
+        logger.info("Reading files to data frame")
+        tables = [None] * len(files)
+        for i, fl in enumerate(files):
+            tables[i] = pandas.read_csv(
+              fl, sep="\t", nrows=1000,
+              usecols=lambda x: x.startswith("f_") or x.startswith("pred"))
+        frame = pandas.concat(tables)
+        sh = frame.shape
+        logger.info("Read data frame of dim ({} x {})".format(sh[0], sh[1]))
 
-def read_matrices(files):
-    logger.info("Reading files into data frame")
-    l = [None] * len(files)
-    for i, fl in enumerate(files):
-        df = pandas.read_csv(
-          fl, sep="\t", nrows=1000,
-          usecols=lambda x: x.startswith("f_") or x.startswith("pred"))
-        l[i] = df
-    df = pandas.concat(l)
-    sh = df.shape
-    logger.info("Read data frame with dimension ({} x {})".format(sh[0], sh[1]))
-    return df
+        return frame
 
+    def compute_silhouettes(self, path, n=100):
+        out_silhouette = path + "-statistics-silhouette.tsv"
+        logger.info("Writing silhouette scores to: {}".format(out_silhouette))
 
-def read_matrix(fl):
-    return pandas.read_csv(fl, sep="\t", nrows=1000,
-                           usecols=lambda x: x.startswith("f_"))
+        files = sample(find_by_suffix(path + "-clusters/cluster*", "tsv"), n)
 
-
-def compute_silhouettes(outfolder):
-    start = time.clock()
-    out_silhouette = outfolder + "-statistics-silhouette.tsv"
-    logger.info("Writing silhouettes to: {}".format(out_silhouette))
-
-    files = [x for x in glob.glob(outfolder + "-clusters/cluster*") \
-             if x.endswith(".tsv")]
-    K = len(files)
-    mat = read_matrices(files)
-    with open(out_silhouette, "w") as ot:
-        logger.info("Opening file IO")
-        ot.write("#Cluster\tNeighbor\tSilhouette\n")
-        for current_idx in range(K):
+        len_f = len(files)
+        mat = self._read_matrices(files)
+        for current_idx in range(len(files)):
             logger.info("Doing file {}".format(current_idx))
-            _compute_silhouette(current_idx, K, ot, mat)
-    logger.info("TIme {}".format(time.clock() - start))
+            self._compute_silhouette(current_idx, len_f, mat)
 
+    def _compute_silhouette(self, current_idx, K, mat):
+        min_cluster, min_distance = self._mp_min_distance(current_idx, K, mat)
+        within_distance = self._mean_distance(current_idx, current_idx, mat)
+        silhouette = (min_distance - within_distance) / \
+                     numpy.maximum(min_distance, within_distance)
+        return zip(min_cluster, silhouette)
 
-def _compute_silhouette(current_idx, K, ot, mat):
-    # np_i = read_matrix(outfiles[i])
-    min_cluster, min_distance = mp_min_distance(current_idx, K, mat)
-    within_distance = _mean_distance(current_idx, current_idx, mat)
-    silhouette = (min_distance - within_distance) / \
-                 numpy.maximum(min_distance, within_distance)
-    for clust, sil in zip(min_cluster, silhouette):
-        ot.write("{}\t{}\t{}".format(current_idx, clust, sil) + "\n")
+    def _mp_min_distance(self, current_idx, K, mat):
+        itr = numpy.array([j for j in range(K) if j != current_idx])
+        distances = [self._mean_distance(it, current_idx, mat) for it in itr]
+        distances = numpy.vstack(distances).T
+        argmins = numpy.argmin(distances, axis=1)
+        min_distances = numpy.min(distances, axis=1)
+        arg = itr[argmins]
+        return arg, min_distances
 
-
-def mp_min_distance(current_idx, K, mat):
-    itr = numpy.array([j for j in range(K) if j != current_idx])
-    distances = [_mean_distance(it, current_idx, mat) for it in itr]
-    distances = numpy.vstack(distances).T
-    argmins = numpy.argmin(distances, axis=1)
-    min_distances = numpy.min(distances, axis=1)
-    arg = itr[argmins]
-    return arg, min_distances
-
-
-def _mean_distance(it, current_idx, df):
-    """Computes distances between np array and np_j"""
-    # np_j = read_matrix(outfiles[j])
-    distances = scipy.spatial.distance.cdist(
-      df[df.prediction == current_idx].iloc[:, 1:],
-      df[df.prediction == it].iloc[:, 1:])
-    return numpy.mean(distances, axis=1)
+    @staticmethod
+    def _mean_distance(it, current_idx, df):
+        distances = scipy.spatial.distance.cdist(
+          df[df.prediction == current_idx].iloc[:, 1:],
+          df[df.prediction == it].iloc[:, 1:])
+        return numpy.mean(distances, axis=1)
 
 
 @click.command()
@@ -122,7 +113,7 @@ def run(path):
             cs = ClusterStatistics(spark)
             gene_pred = cs.count_statistics(data, ["gene", "prediction"])
             path_pred = cs.count_statistics(data, ["pathogen", "prediction"])
-            statistics.write_files(outpath)
+            cs.compute_silhouettes(data)
         except Exception as e:
             logger.error("Some error: {}".format(str(e)))
 
