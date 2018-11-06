c05edcb6045c723ede4c05289861c801d18f2166
dirmeier <simon.dirmeier@bsse.ethz.ch>
2018-11-06 15:04:15 +0100

    Adds logreg test

--- koios/fit/glm_fit.py
+++ koios/fit/glm_fit.py
@@ -25,6 +25,8 @@ import pandas
 
 import scipy as sp
 
+from koios.globals import GAUSSIAN_, BINOMIAL_
+
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
@@ -34,22 +36,40 @@ class GLMFit:
         self.__data = data
         self.__model = model
         self.__response = response
-        self.__df = model.summary.degreesOfFreedom
-        self.__mse = model.summary.meanSquaredError
-        self.__r2 = model.summary.r2
-        self.__rmse = model.summary.rootMeanSquaredError
-        self.__table = pandas.DataFrame(
-          {"beta": sp.append(sp.array(model.intercept),
-                             sp.array(model.coefficients)),
-           "p_values": model.summary.pValues,
-           "t_values": model.summary.tValues,
-           "se": model.summary.coefficientStandardErrors
-           }
-        )
+        if response == GAUSSIAN_:
+            self.__df = model.summary.degreesOfFreedom
+            self.__mse = model.summary.meanSquaredError
+            self.__r2 = model.summary.r2
+            self.__rmse = model.summary.rootMeanSquaredError
+            self.__table = pandas.DataFrame(
+              {"beta": sp.append(sp.array(model.intercept),
+                                 sp.array(model.coefficients)),
+               "p_values": model.summary.pValues,
+               "t_values": model.summary.tValues,
+               "se": model.summary.coefficientStandardErrors
+               })
+        else:
+            self.__accuracy = model.summary.accuracy
+            self.__auc = model.summary.areaUnderROC
+            self.__pr = model.summary.pr.toPandas()
+            self.__roc = model.summary.roc.toPandas()
+            self.__table = pandas.DataFrame({
+                "beta": sp.append(sp.array(model.intercept),
+                                  sp.array(model.coefficients))}
+            )
+            self.__measures = pandas.DataFrame({
+                "f_measure": model.summary.fMeasureByLabel,
+                "fpr": model.falsePositiveRateByLabel,
+                "precision": model.precisionByLabel,
+                "recall": model.recallByLabel,
+                "tpr": model.truePositiveRateByLabel
+            })
 
     def write_files(self, outfolder):
         self._write_stats(outfolder)
         self._write_table(outfolder)
+        if self.__response == BINOMIAL_:
+            self._write_binomial_measures(outfolder)
 
     def _write_table(self, outfolder):
         self.__table.to_csv(outfolder + "-table.tsv",
@@ -58,10 +78,24 @@ class GLMFit:
     def _write_stats(self, outfolder):
         out_file = outfolder + "-statistics.tsv"
         with open(out_file, "w") as fh:
-            fh.write("{}\t{}\t{}\t{}\t{}\n".format(
-              "response", "df", "mse", "r2", "rmse"))
-            fh.write("{}\t{}\t{}\t{}\t{}\n".format(
-              self.__response, self.__df, self.__mse, self.__r2, self.__rmse))
+            if self.__response == BINOMIAL_:
+                fh.write("{}\t{}\t{}\n".format("response", "accuracy", "auc"))
+                fh.write("{}\t{}\t{}\n".format(
+                  self.__response, self.__accuracy, self.__auc))
+            else:
+                fh.write("{}\t{}\t{}\t{}\t{}\n".format(
+                  "response", "df", "mse", "r2", "rmse"))
+                fh.write("{}\t{}\t{}\t{}\t{}\n".format(
+                  self.__response, self.__df, self.__mse, self.__r2,
+                  self.__rmse))
+
+    def _write_binomial_measures(self, outfolder):
+        self.__pr.to_csv(outfolder + "-precision_recall.tsv",
+                         sep="\t", index=False, header=True)
+        self.__roc.to_csv(outfolder + "-roc_curve.tsv",
+                          sep="\t", index=False, header=True)
+        self.__measures.to_csv(outfolder + "-measures.tsv",
+                               sep="\t", index=False, header=True)
 
     @property
     def response(self):
