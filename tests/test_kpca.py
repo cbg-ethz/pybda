19e0cfd1e75751579654f55ff81cb6e72f4f3392
Simon Dirmeier <simon.dirmeier@web.de>
2019-02-12 20:47:20 +0100

    Adds couple iof test files

--- tests/test_kpca.py
+++ tests/test_kpca.py
@@ -18,40 +18,50 @@
 # @author = 'Simon Dirmeier'
 # @email = 'simon.dirmeier@bsse.ethz.ch'
 
+import unittest
+
 import numpy
 import pandas
-import sklearn.decomposition
+import pyspark
+
 from sklearn import datasets
+import sklearn.decomposition
 
 from pybda.factor_analysis import FactorAnalysis
 from pybda.globals import FEATURES__
 from pybda.spark.features import split_vector
-from tests.test_dimred_api import TestDimredAPI
 
 
-class TestFA(TestDimredAPI):
+class TestKPCA(unittest.TestCase):
     """
     Tests the facor analysis API
     """
 
     def setUp(self):
-        super().setUp()
+        unittest.TestCase.setUp(self)
+        self.spark = (pyspark.sql.SparkSession.builder
+                      .master("local")
+                      .appName("unittest")
+                      .config("spark.driver.memory", "3g")
+                      .config("spark.executor.memory", "3g")
+                      .getOrCreate())
         iris = datasets.load_iris()
         self.X = iris.data[:, :4]
         y = iris.target
         self.features = ["sl", "sw", "pl", "pw"]
         df = pandas.DataFrame(data=numpy.column_stack((self.X, y)),
-                              columns=self.features + ["species"])
+                                   columns=self.features + ["species"])
         self.spark_df = self.spark.createDataFrame(df)
         self.fa = FactorAnalysis(self.spark, 2, self.features)
 
         self.skfa = sklearn.decomposition.FactorAnalysis(2, max_iter=25)
 
     def tearDown(self):
-        super().tearDown()
+        self.spark.stop()
 
     def test_fa(self):
         fit = self.fa.fit_transform(self.spark_df)
         df = (split_vector(fit.data, FEATURES__))[["f_0", "f_1"]]
         df = df.toPandas().values
         skfit = self.skfa.fit_transform(self.X)
+
