29702e779dc9b4fc5ff699a268ecfde00d7b1ddd
Simon Dirmeier <simon.dirmeier@web.de>
2019-02-22 14:59:25 +0100

    Updated LDA

--- pybda/fit/lda_transform.py
+++ pybda/fit/lda_transform.py
@@ -24,25 +24,28 @@ import os
 
 from pandas import DataFrame
 
-from pybda.fit.dimension_reduction_fit import DimensionReductionFit
+from pybda.fit.dimension_reduction_transform import DimensionReductionTransform
+from pybda.globals import FEATURES__
 from pybda.io.io import mkdir
+from pybda.plot.descriptive import scatter, histogram
 from pybda.plot.dimension_reduction_plot import biplot, \
     plot_cumulative_variance
+from pybda.sampler import sample
+from pybda.spark.features import split_vector
 from pybda.stats.stats import normalized_cumsum
+from pybda.util.cast_as import as_pandas
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-class LDAFit(DimensionReductionFit):
-    def __init__(self,  n_components, loadings, vars, features, response):
-        super().__init__(n_components, features, loadings)
-        self.__vars = vars
-        self.__response = response
+class LDATransform(DimensionReductionTransform):
+    def __init__(self, data, model):
+        super().__init__(data, model)
 
     @property
     def response(self):
-        return self.__response
+        return self.model.response
 
     @property
     def n_discriminants(self):
@@ -61,18 +64,21 @@ class LDAFit(DimensionReductionFit):
         return self.__vars
 
     def write(self, outfolder):
-        self._write_loadings(outfolder + "-projection.tsv")
+        logger.info("Writing transform")
+        self.model.write(outfolder)
+        self.write_tsv(outfolder)
         plot_fold = outfolder + "-plot"
         mkdir(plot_fold)
         self._plot(os.path.join(plot_fold, "linear_discriminant_analysis"))
 
     def _plot(self, outfile):
         logger.info("Plotting")
-        cev = normalized_cumsum(self.variances)
+        subsamp = as_pandas(
+            split_vector(sample(self.data, 10000), FEATURES__))
         for suf in ["png", "pdf", "svg", "eps"]:
-            plot_cumulative_variance(
-                outfile + "-discriminants-explained_variance." + suf, cev,
-                "# discriminants")
-            biplot(outfile + "-loadings-biplot." + suf,
-                   DataFrame(self.loadings, columns=self.feature_names),
-                   "Discriminant 1", "Discriminant 2")
+            scatter(outfile + "-scatter_plot." + suf, subsamp, "f_0", "f_1",
+                    "Discriminant 1", "Discriminant 2", color=self.response)
+            for i in map(lambda x: "f_" + str(x),
+                         range(min(10, self.n_discriminants))):
+                histogram(outfile + "-histogram_{}.".format(i) + suf,
+                          subsamp[i].values, i)
