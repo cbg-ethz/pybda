fe94e86e9be2c79e95ec45eae19020568101fde6
dirmeier <simon.dirmeier@bsse.ethz.ch>
2019-03-26 22:51:56 +0100

    Beautifies some methods and collapses code

--- pybda/config/config_checks.py
+++ pybda/config/config_checks.py
@@ -33,7 +33,7 @@ from pybda.globals import (
 REQUIRED_ARGS = [SPARK__, INFILE__, OUTFOLDER__, META__, FEATURES__]
 METHOD_REQUIRED_ARGS = collections.OrderedDict(
   [(DIM_RED__, REQUIRED_ARGS + [DIM_RED__, N_COMPONENTS__]),
-   (CLUSTERING__, REQUIRED_ARGS + [CLUSTERING__, N_CENTERS__])
+   (CLUSTERING__, REQUIRED_ARGS + [CLUSTERING__, N_CENTERS__]),
    (REGRESSION__, REQUIRED_ARGS + [REGRESSION__, FAMILY__, RESPONSE__])]
 )
 ALGORITHM_REQUIRED_ARGS = collections.OrderedDict(
@@ -41,15 +41,26 @@ ALGORITHM_REQUIRED_ARGS = collections.OrderedDict(
 )
 
 
-def check_args(config, method):
-    print("Checking arguments for method: {}".format(method))
+def _check_missing_arg(config, arg):
+    if arg not in config.keys():
+        print("\033[1;31mMissing argument in config file. "
+            "Could not find argument: {} \033[0m".format(arg))
+        exit(-1)
+
+
+def _check_args(config, method):
     reg_args = METHOD_REQUIRED_ARGS[method]
     if config[method] in ALGORITHM_REQUIRED_ARGS.keys():
         reg_args = reg_args + ALGORITHM_REQUIRED_ARGS[LDA__]
     for reg_arg in reg_args:
-        if reg_arg not in config.keys():
-            print("Missing argument in config file. "
-                  "Could not find argument: {}".format(reg_arg))
-            exit(-1)
+        _check_missing_arg(config, reg_arg)
+
+
+def check_args(config, method):
+    print("Checking command line arguments for method: {}".format(method))
+    _check_missing_arg(config, method)
+    for alg in [DIM_RED__, CLUSTERING__, REGRESSION__]:
+        if alg in config.keys():
+            _check_args(config, alg)
 
 
