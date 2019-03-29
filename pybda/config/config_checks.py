5b76866ac3a7128ef82377a089e044de0f161ad6
Simon Dirmeier <simon.dirmeier@web.de>
2019-03-29 15:59:14 +0100

    Adds contributing guide and some minor updates

--- pybda/config/config_checks.py
+++ pybda/config/config_checks.py
@@ -18,33 +18,24 @@
 # @author = 'Simon Dirmeier'
 # @email = 'simon.dirmeier@bsse.ethz.ch'
 
-
 import collections
-from pybda.globals import (
-    DIM_RED__, REGRESSION__, INFILE__, SPARK__,
-    OUTFOLDER__, FEATURES__, CLUSTERING__,
-    N_COMPONENTS__, N_CENTERS__, META__,
-    RESPONSE__,
-    FAMILY__,
-    LDA__
-)
-
+from pybda.globals import (DIM_RED__, REGRESSION__, INFILE__, SPARK__,
+                           OUTFOLDER__, FEATURES__, CLUSTERING__,
+                           N_COMPONENTS__, N_CENTERS__, META__, RESPONSE__,
+                           FAMILY__, LDA__)
 
 REQUIRED_ARGS = [SPARK__, INFILE__, OUTFOLDER__, META__, FEATURES__]
 METHOD_REQUIRED_ARGS = collections.OrderedDict(
-  [(DIM_RED__, REQUIRED_ARGS + [DIM_RED__, N_COMPONENTS__]),
-   (CLUSTERING__, REQUIRED_ARGS + [CLUSTERING__, N_CENTERS__]),
-   (REGRESSION__, REQUIRED_ARGS + [REGRESSION__, FAMILY__, RESPONSE__])]
-)
-ALGORITHM_REQUIRED_ARGS = collections.OrderedDict(
-  [(LDA__, [RESPONSE__])]
-)
+    [(DIM_RED__, REQUIRED_ARGS + [DIM_RED__, N_COMPONENTS__]),
+     (CLUSTERING__, REQUIRED_ARGS + [CLUSTERING__, N_CENTERS__]),
+     (REGRESSION__, REQUIRED_ARGS + [REGRESSION__, FAMILY__, RESPONSE__])])
+ALGORITHM_REQUIRED_ARGS = collections.OrderedDict([(LDA__, [RESPONSE__])])
 
 
 def _check_missing_arg(config, arg):
     if arg not in config.keys():
         print("\033[1;31mMissing argument in config file. "
-            "Could not find argument: {} \033[0m".format(arg))
+              "Could not find argument: {} \033[0m".format(arg))
         exit(-1)
 
 
@@ -62,5 +53,3 @@ def check_args(config, method):
     for alg in [DIM_RED__, CLUSTERING__, REGRESSION__]:
         if alg in config.keys():
             _check_args(config, alg)
-
-
